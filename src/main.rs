aba8fd7a5080d29c02f4b779f09b43f1eddeb83f
Philip Rodrigues <phil@phiroict.co.nz>
2023-07-22 13:18:31 +1200

    Updates and adapt path

--- src/main.rs
+++ src/main.rs
@@ -81,6 +81,7 @@ fn main() -> io::Result<()> {
         t.join().expect("Could not join thread");
     }
     // Change your destination path in here.
+    print!("Download complete, starting to move to NAS");
     let move_result = move_to_nas(folder_name.clone(), format!("/home/phiro/mounts/Volume_1/youtube/{}", &folder_name));
     if move_result {
         println!("Move complete")
