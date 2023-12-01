f78c343e8ece695d747c4e9072a57d4e9bfc2fcb
Philip Rodrigues <phil@phiroict.co.nz>
2023-12-01 14:40:45 +1300

    Pass debug level as a parameter defaults to 'Warn'

--- src/logging.rs
+++ src/logging.rs
@@ -1,11 +1,12 @@
 use chrono::Local;
 use env_logger::Builder;
 use log::{trace, LevelFilter};
-use std::io::Write;
+use std::{io::Write, str::FromStr};
 use uuid::Uuid;
 
-pub fn initialize_logging() {
+pub fn initialize_logging(log_level: String) {
     let request_id = Uuid::new_v4();
+    let level = LevelFilter::from_str(log_level.as_str()).unwrap_or(LevelFilter::Trace);
     Builder::new()
         .format(move |buf, record| {
             writeln!(
@@ -17,7 +18,7 @@ pub fn initialize_logging() {
                 record.args()
             )
         })
-        .filter(None, LevelFilter::Trace)
+        .filter(None, level)
         .init();
     trace!("Logging system set up.")
 }
